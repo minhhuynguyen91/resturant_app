b16d961fa1a3fbb8a180e1e2ae62ed8e569573fa
Minh Nguyen <ng.huyminh91@gmail.com>
2017-02-14 20:56:46 +0700

    Add order card info
    * Add total price (without delivery)
    * Add datetime for the order
    
    Signed-off-by: Minh Nguyen <ng.huyminh91@gmail.com>

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,13 +10,24 @@ class OrdersController < ApplicationController
     @order = @food_item.orders.build(order_params)
     if @order.save
       flash[:success] = "Thank you for your order."
-      redirect_to root_path
+      redirect_to food_item_order_path(@food_item, @order)
     else
       render 'new'
     end
   end
   
+  def index
+    @food_item = FoodItem.find(params[:food_item_id])
+    @orders = @food_item.orders.all
+  end
   
+  def show
+    @food_item = FoodItem.find(params[:food_item_id])
+    @order = @food_item.orders.find(params[:id])
+    @date_time = DateTime.parse(@order.created_at.to_s)
+  end
+
+
   private
     def order_params
       params.require(:order).permit(:quantity, :customer_name, :phone_number)
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170214111215) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_sections_on_name", using: :btree
   end
 
   add_foreign_key "food_items", "sections"
